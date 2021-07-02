093b257c757dea743cddd811b24246386cea59d4
hieurose <67453388+hieusnguyen0709@users.noreply.github.com>
2021-07-02 13:29:39 +0700

    main
    main

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,25 @@
+<!DOCTYPE HTML>
+<head>
+<title>HNShop</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+<link type="image/png" rel="shortcut icon" href="images/logoHN.png"/>
+<script src="js/jquerymain.js"></script>
+<script src="js/script.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script> 
+<script type="text/javascript" src="js/nav.js"></script>
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script> 
+<script type="text/javascript" src="js/nav-hover.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
+<script type="text/javascript">
+  $(document).ready(function($){
+    $('#dc_mega-menu-orange').dcMegaMenu({rowItems:'4',speed:'fast',effect:'fade'});
+  });
+</script>
 <?php
 include 'lib/session.php';
 Session::init();
@@ -26,28 +48,6 @@ $product = new product();
   header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
   header("Cache-Control: max-age=2592000");
 ?>
-<!DOCTYPE HTML>
-<head>
-<title>HNShop</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
-<link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
-<link type="image/png" rel="shortcut icon" href="images/logoHN.png" />
-<script src="js/jquerymain.js"></script>
-<script src="js/script.js" type="text/javascript"></script>
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script> 
-<script type="text/javascript" src="js/nav.js"></script>
-<script type="text/javascript" src="js/move-top.js"></script>
-<script type="text/javascript" src="js/easing.js"></script> 
-<script type="text/javascript" src="js/nav-hover.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
-<script type="text/javascript">
-  $(document).ready(function($){
-    $('#dc_mega-menu-orange').dcMegaMenu({rowItems:'4',speed:'fast',effect:'fade'});
-  });
-</script>
 </head>
 <body>
   <div class="wrap">
